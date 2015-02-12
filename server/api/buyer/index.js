92e2695914c7de7a099c8eb6b076b1cb45d0ecd7
peerwit <pio@PIOs-MacBook-Air.local>
2015-02-12 15:05:41 -0800

    Update routes

--- server/api/buyer/index.js
+++ server/api/buyer/index.js
@@ -7,8 +7,6 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.hasRole('admin'), controller.index);
-router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
