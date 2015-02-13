df712fa511e3f4ffe99043339be214c64800a4c6
Scott Rice <scottrice10@gmail.com>
2015-02-12 23:12:24 -0800

    Revert "Photo upload directive and vendor (product upload screen)"
    This reverts commit a9e63bc4253a42656822d928d69147d44a1fe714.

--- client/app/vendor/vendor.controller.js
+++ client/app/vendor/vendor.controller.js
@@ -1,48 +1,6 @@
 'use strict';
 
 angular.module('thesisApp')
-  .controller('VendorCtrl', function ($scope, User, Auth) {
-    $scope.errors = {};
-    $scope.minPasswordLength = 3;
-    Auth.getCurrentUser().then(function(user) {
-      $scope.user = user;
-      $scope.isWorker = ($scope.user.account_type === 'Worker');
-    });
-
-    $scope.updateProfile = function(form) {
-      if(form.$valid) {
-        var updates = {
-          name: $scope.user.name,
-          location: $scope.user.location,
-          email: $scope.user.email,
-          img_url: Auth.getImages(),
-          summary: $scope.user.summary,
-          hourly_rate: $scope.user.hourly_rate,
-          skills: $scope.user.skills,
-          accountType: $scope.user.account_type
-        };
-
-        return Auth.editProfile(updates)
-          .then(function(user) {
-            $scope.message = 'Profile successfully updated.'
-          })
-          .catch(function(err) {
-            $scope.message = 'Error updating profile. Please try again later.';
-          });
-      }
-    };
-
-    $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword($scope.user.oldPassword, $scope.user.newPassword)
-          .then(function() {
-            $scope.message = 'Password successfully changed.';
-          })
-          .catch(function() {
-            $scope.errors.other = 'Incorrect password';
-            $scope.message = '';
-          });
-      }
-    };
+  .controller('VendorCtrl', function ($scope) {
+    $scope.message = 'Hello';
   });
