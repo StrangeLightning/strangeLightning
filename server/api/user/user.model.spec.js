6413ffa5b349164944b6032002e738b74e8304a2
Scott Rice <scottrice10@gmail.com>
2015-02-14 16:53:45 -0800

    Get tests working. Removed unused authentication methods.

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var should = require('should');
-var app = require('../../app');
+var app = require('../../../server.js');
 var User = require('./user.model.js');
 
 var user = new User({
