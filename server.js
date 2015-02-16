0626e2d4d2ff0e51209a4b53882b856dbb341129
peerwit <pio@PIOs-MacBook-Air.local>
2015-02-15 19:40:17 -0800

    use https and http

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 var config = require('./server/config/environment');
 var https = require('https');
 var fs = require('fs');
-var credentials = {key: fs.readFileSync('./shared/config/ssl.key', 'utf-8'), cert: fs.readFileSync('./shared/config/ssl.crt', 'utf-8')};
+var credentials = {key: fs.readFileSync('./../shared/config/ssl.key', 'utf-8'), cert: fs.readFileSync('./../shared/config/ssl.crt', 'utf-8')};
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -28,15 +28,16 @@ var socketio = require('socket.io')(serverHTTPS, {
   serveClient: (config.env === 'production') ? false : true,
   path: '/socket.io-client'
 });
-app.all('*', function(req,res){
-	console.log('asdf');
+app.all('*', function(req,res, next){
+	if (!req.connection.encrypted) {res.redirect('https://sphereable.com')}
+	else {next();}
 })
 require('./server/config/socketio')(socketio);
 require('./server/config/express')(app);
 require('./server/routes')(app);
 
 // Start server HTTPS
-serverHTTPS.listen(config.port, config.ip, function () {
+serverHTTPS.listen(443, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
