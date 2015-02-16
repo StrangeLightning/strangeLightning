6a68550919826c4c7630cf1b6f68dff7d85bcfbc
peerwit <pio@PIOs-MacBook-Air.local>
2015-02-16 09:03:44 -0800

    use http and https for local and production

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ var mongoose = require('mongoose');
 var config = require('./server/config/environment');
 var https = require('https');
 var fs = require('fs');
-var credentials = {key: fs.readFileSync('./../shared/config/ssl.key', 'utf-8'), cert: fs.readFileSync('./../shared/config/ssl.crt', 'utf-8')};
+console.log(process.env.NODE_ENV);
+var credentials = {
+			key: process.env.NODE_ENV === 'production' ? fs.readFileSync('../shared/config/ssl.key', 'utf-8') : fs.readFileSync('./shared/config/ssl.key', 'utf-8'),
+			cert: process.env.NODE_ENV === 'production' ? fs.readFileSync('../shared/config/ssl.crt', 'utf-8') : fs.readFileSync('./shared/config/ssl.crt', 'utf-8')
+		};
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -37,14 +41,17 @@ require('./server/config/express')(app);
 require('./server/routes')(app);
 
 // Start server HTTPS
-serverHTTPS.listen(443, config.ip, function () {
+serverHTTPS.listen(process.env.NODE_ENV === 'production' ? 443 : config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
-serverHTTP.listen(80, config.ip, function () {
-  // console.log(req.connection.encrypted?true:false);
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+if (process.env.NODE_ENV === 'production') {
+	serverHTTP.listen(80, config.ip, function () {
+	  // console.log(req.connection.encrypted?true:false);
+	  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+	});
+}
+
 
 
 // Expose app
