6c95734cf703904fbdb140434475d393aa3ab095
peerwit <pio@PIOs-MacBook-Air.local>
2015-02-16 15:56:42 -0800

    force https

--- server/api/amazon/index.js
+++ server/api/amazon/index.js
@@ -6,25 +6,25 @@ var config = require('../../config/environment');
 var auth = require('../../auth/auth.service');
 
 var util = require('util'),
-    OperationHelper = require('apac').OperationHelper;
- 
+OperationHelper = require('apac').OperationHelper;
+
 function test(req, res, next){
-	// console.log(config.amazon.clientID, "asdf");
-	var opHelper = new OperationHelper({
-	    awsId:     config.amazon.clientID,
-	    awsSecret: config.amazon.clientSecret,
-	    assocId:   config.amazon.clientAccount 
-	});
-	console.log(config.amazon.clientID);
-	opHelper.execute('ItemSearch', {
-		  'Keywords': req.body.term,
-		  'SearchIndex': 'Blended',
-		  'ResponseGroup': 'Similarities,ItemIds,Images,Small'
-		}, function(err, results) {
-			console.log(results);
+  // console.log(config.amazon.clientID, "asdf");
+  var opHelper = new OperationHelper({
+  	1awsId:     config.amazon.clientID,
+  	awsSecret: config.amazon.clientSecret,
+  	assocId:   config.amazon.clientAccount 
+  });
+  console.log(config.amazon.clientID);
+  opHelper.execute('ItemSearch', {
+  	'Keywords': req.body.term,
+  	'SearchIndex': 'Blended',
+  	'ResponseGroup': 'Similarities,ItemIds,Images,Small'
+  }, function(err, results) {
+  	console.log(results);
 		// var r = results.ItemSearchResponse.Items[0].Item[0];
 	    // var r = JSON.stringify(results.ItemSearchResponse);
-	    	res.end(JSON.stringify(results));
+	    res.end(JSON.stringify(results));
 	 //    opHelper.execute('ItemLookup', {
 		//   'ItemId': r.Item[0].ASIN,
 		//   'ResponseGroup': 'Images'
@@ -33,7 +33,7 @@ function test(req, res, next){
 		//     // var r = JSON.stringify(results.ItemSearchResponse);
 		//     res.end(JSON.stringify(results));
 		// });
-	});
+});
 
 }
 var router = express.Router();
