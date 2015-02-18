ee47ed3929bb45924873206fbe4214e7c9672264
Kevin Primat <primatie@primatie.com>
2015-02-17 23:42:42 -0800

    Repair VendorCtrl

--- client/app/vendor/vendor.controller.spec.js
+++ client/app/vendor/vendor.controller.spec.js
@@ -5,7 +5,7 @@ describe('Controller: AboutCtrl', function () {
   // load the controller's module
   beforeEach(module('thesisApp'));
 
-  var AboutCtrl, scope;
+  var VendorCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
