23fdf6faa4fd22f0580a65bc4e5e1d9c789ba00d
Tole <Tole>
2015-02-17 18:20:57 -0800

    removed test buttons

--- server/api/cart/cart.controller.js
+++ server/api/cart/cart.controller.js
@@ -13,29 +13,30 @@ exports.index = function(req, res) {
   });
 };
 
-
-
 // Get a single cart
 exports.show = function(req, res) {
+  //query parameter
   var query = Cart.where({
     'userName': req.params.userName
   });
-  console.log("IN SHOW username: ", req.params.userName)
+
+  //find matching query
   query.findOne(function(err, cart) {
     if (err) {
-      console.log("ERROR FROM SHOW: ", err)
+      console.log("ERROR FROM cart.controller: ", err);
       return handleError(res, err);
     }
     if (!cart) {
-      console.log('searched but no cart in db')
+      console.log('searched but no cart in db');
       return res.send(404);
     }
-    console.log("CARTATSHOW", cart)
+    console.log("cart on server: ", cart);
+    //return queried cart
     return res.json(cart);
   });
 };
 
-// Creates a new cart in the DB.
+// Creates a new cart in the DB
 exports.create = function(req, res) {
   var cart = new Cart({
     'userName': req.params.userName,
@@ -44,63 +45,41 @@ exports.create = function(req, res) {
   Cart.create(req.body, function(err, cart) {
     if (err) {
 
-      console.log('ERROR IN CREATE ON SERVER SIDE', err)
+      console.log('ERROR IN CREATE CART CONTROLLER:  ', err)
       return handleError(res, err);
     } else {
-      console.log("IN server no error", cart)
+      console.log("creating cart: ", cart)
       return res.json(201, cart);
     }
   });
 };
 
-// Updates an existing cart in the DB.
+// Updates an existing cart in the DB, specifically items
 exports.update = function(req, res) {
-  // if (req.body.id) {
-  //   console.log('DELETING BODY ID IN UPDATE')
-  //   delete req.body.id;
-  // }
-  console.log(req.body, "REQ BODY")
-  console.log(req.params, "params")
-  var cart = new Cart({
-    'userName': req.params.userName,
-    'items': req.body
-
-  })
-  console.log("CART", cart)
-    // upsertCart = cart.toObject()
-    // delete upsertCart._id
-  console.log("IN EXPORTS UPDATE")
-
+  //queries the username
   var query = Cart.where({
     'userName': req.params.userName
   });
+  //find a matching query(arg1), updates items (arg 2), upsert: if new row make new row (option in arg 3)
   Cart.findOneAndUpdate(query, {
     'items': req.body
   }, {
     upsert: true
   }, function(err, cart) {
-    console.log(cart, "# of rows updated")
+    console.log(cart, "row updated");
     if (err) {
-      console.log("EROOR IN SERVERSIDE UPDATE")
+      console.log("ERROR in cart.controller(serverside): ", err)
       return handleError(res, err);
     }
     if (!cart) {
       return res.send(404);
     }
-    // Cart.save(function(err) {
-    //   if (err) {
-    //     console.log("EROOR IN SERVERSIDE UPDATE #2")
-
-    //     return handleError(res, err);
-    //   }
-    //   return res.json(200, cart);
-    // });
   });
 };
 
-// Deletes a cart from the DB.
+// Deletes a cart from the DB, removes user from cart schema as well.
 exports.destroy = function(req, res) {
-  //searches by username which is appended to the file end in serverr
+  //searches by username
   Cart.findOneAndRemove({
     'userName': req.params.userName
   }, function(err, cart) {
@@ -108,10 +87,10 @@ exports.destroy = function(req, res) {
       return handleError(res, err);
     }
     if (!cart) {
-      console.log('inside server side destroy')
+      console.log('inside server side destroy');
       return res.send(404);
     }
-    console.log("CART TO BE REMOVED ON SERVERSIDE:  ", cart)
+    console.log("CART TO BE REMOVED ON SERVERSIDE:  ", cart);
     cart.remove(function(err) {
       if (err) {
         return handleError(res, err);
