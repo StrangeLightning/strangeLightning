01cc61601e020c46b0bc49eb5c7632222d49ff6e
Scott Rice <scottrice10@gmail.com>
2015-02-17 19:49:52 -0800

    Add 'Add to Cart' button to catalogue.

--- client/app/catalog/catalog.controller.js
+++ client/app/catalog/catalog.controller.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('thesisApp')
-  .controller('CatalogCtrl', function($scope, $http) {
-    $scope.addToCart = function(item) {
-      consol.log(item)
-    }
+  .controller('CatalogCtrl', function($scope, cartFactory, $http) {
+    $scope.addToCart = cartFactory.addItem;
+
+    $scope.getImage = function(product){
+      var img = ;
+      if(product.mediumImage){
+        img = product.mediumImage;
+      }
+    };
+
+    //init
+    $http.get('/api/products/').
+      success(function(data) {
+        $scope.products = data;
+      }).
+      error(function(err) {
+        console.log(err);
+      });
   });
