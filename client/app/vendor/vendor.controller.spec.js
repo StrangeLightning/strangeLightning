b6fd032d3bcb8c5dcfe57bd51474b909fa5800aa
Kevin Primat <primatie@primatie.com>
2015-02-17 23:42:58 -0800

    Repair VendorCtrl

--- client/app/vendor/vendor.controller.spec.js
+++ client/app/vendor/vendor.controller.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: VendorCtrl', function () {
 
   // load the controller's module
   beforeEach(module('thesisApp'));
