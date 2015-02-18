1e5d120505bf4cef208f3ade3562175e7c1915f2
Scott Rice <scottrice10@gmail.com>
2015-02-17 21:11:41 -0800

    (#98) Products showing on catalogue view.

--- client/app/catalog/catalog.controller.js
+++ client/app/catalog/catalog.controller.js
@@ -5,16 +5,18 @@ angular.module('thesisApp')
     $scope.addToCart = cartFactory.addItem;
 
     $scope.getImage = function(product){
-      var img = ;
+      var img = "https://s3-eu-west-1.amazonaws.com/petrus-blog/placeholder.png";
       if(product.mediumImage){
         img = product.mediumImage;
       }
+
+      return img;
     };
 
     //init
-    $http.get('/api/products/').
-      success(function(data) {
-        $scope.products = data;
+    $http.post('/api/amazon-products/').
+      success(function(results) {
+        $scope.products = results.data;
       }).
       error(function(err) {
         console.log(err);
