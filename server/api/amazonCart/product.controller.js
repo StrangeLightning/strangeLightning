420a9c3d1279c08817fcf78200af15aaab939caa
peerwit <pio@PIOs-MacBook-Air.local>
2015-02-18 20:04:34 -0800

    error check for CartCreate

--- server/api/amazonCart/product.controller.js
+++ server/api/amazonCart/product.controller.js
@@ -24,7 +24,10 @@ exports.createCart = function(req, res, next) {
       'Item.1.Quantity': '1',
     }, function(err, results) {
       var _results = [];
-      if (results.CartCreateResponse && results.CartCreateResponse.Cart) {
+      if (results.request && results.request.Errors) {
+        res.end(JSON.stringify('Something went wrong! Here is a snippet: ' + results.request.Errors));
+      }
+      else if (results.CartCreateResponse && results.CartCreateResponse.Cart) {
         var cart = results.CartCreateResponse.Cart[0];
         if (req.user) {
           var user = req.user;
