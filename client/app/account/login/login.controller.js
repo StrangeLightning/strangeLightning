fc86d8093f08b683cd6a109b774c4c21b2f38640
Tole <Tole>
2015-02-23 15:52:41 -0800

    ling conflicts resolved

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -12,6 +12,30 @@ angular.module('thesisApp')
       height: block - navbar - 1
     });
 
+    // $scope.close = function() {
+    //   $('#cart').animate({
+    //     'margin-right': '-=1000'
+    //   }, 500);
+    // }
+
+    $scope.login = function(form) {
+      $scope.submitted = true;
+
+      if (form.$valid) {
+        Auth.login({
+            email: $scope.user.email,
+            password: $scope.user.password
+          })
+          .then(function() {
+            // Logged in, redirect to home
+            $location.path('/');
+          })
+          .catch(function(err) {
+            $scope.errors.other = err.message;
+          });
+      }
+    };
+
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
