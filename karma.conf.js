4572eca4a49bfbcb42c34fca00d81d8a7012c9db
Tole <Tole>
2015-02-24 19:48:45 -0800

    karma conf added progress

--- karma.conf.js
+++ karma.conf.js
@@ -22,8 +22,6 @@ module.exports = function(config) {
       'client/bower_components/lodash/dist/lodash.compat.js',
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
-      'client/bower_components/angular-ui-select/dist/select.js',
-      'client/bower_components/angular-utils-pagination/dirPagination.js',
       'client/app/app.js',
       'client/app/**/*.js',
       'client/components/**/*.js',
