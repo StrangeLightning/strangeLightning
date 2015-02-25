3d5294fb318172626ed78ced0c2a1fc3b8d20f2f
Scott Rice <scottrice10@gmail.com>
2015-02-25 11:25:14 -0800

    (#185) Begin facets

--- client/app/catalog/catalog.factory.js
+++ client/app/catalog/catalog.factory.js
@@ -1,36 +1,36 @@
 'use strict';
 
 angular.module('thesisApp')
-  .factory('catalogFactory', ['$location', '$http', '$rootScope', function($location, $http, $rootScope) {
+  .factory('catalogFactory', ['$location', '$http', '$rootScope', function ($location, $http, $rootScope) {
     var catalog = {};
-    catalog.viewItem = function() {
+    catalog.viewItem = function () {
       $location.path('/product');
     };
 
-    catalog.doSearch = function(searchTerm, pageNumber, filters, callback) {
+    catalog.doSearch = function (searchTerm, pageNumber, filters, callback) {
       $rootScope.$broadcast('search-in-progress');
       return $http.post('/api/amazonproducts/', {
-        q: searchTerm,
-        facets: 'category,price',
-        start: pageNumber,
-        filters: filters
-      })
-        .success(function(results) {
+          q: searchTerm,
+          facets: 'category,price',
+          start: pageNumber,
+          filters: filters
+        })
+        .success(function (results) {
           callback(results.data);
         })
-        .error(function(err) {
+        .error(function (err) {
           console.log(err);
         });
     };
 
-    catalog.doSuggestor = function(searchTerm, callback) {
+    catalog.doSuggestor = function (searchTerm, callback) {
       return $http.post('/api/amazonproducts/suggest/', {
-        q: searchTerm
-      })
-        .success(function(results) {
+          q: searchTerm
+        })
+        .success(function (results) {
           callback(results.data);
         })
-        .error(function(err) {
+        .error(function (err) {
           console.log(err);
         });
     };
