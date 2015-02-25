750073770cc5db19c574b965d7903cba31a6b7f1
Scott Rice <scottrice10@gmail.com>
2015-02-25 10:41:36 -0800

    (#171) Connect Add cart to product view and review 'Get Cart Items' button from catalog

--- client/app/product/product.controller.js
+++ client/app/product/product.controller.js
@@ -1,12 +1,22 @@
 'use strict';
 
 angular.module('thesisApp')
-  .controller('ProductCtrl', ['$scope', 'catalogFactory', function($scope, catalogFactory) {
+  .controller('ProductCtrl', ['$rootScope', '$scope', 'catalogFactory', function($rootScope, $scope, catalogFactory) {
     // This should go into a directive but since it's only 6 lines of code
     // we can leave this here and migrate it inside of a directive should
     // we have a need to create one.
+
     $scope.product = catalogFactory.product;
     //$scope.product.prodAttributes = JSON.parse($scope.product.prodAttributes);
+
+    $scope.addToCart = function (product) {
+      $rootScope.$broadcast('addToCart');
+      if (cartFactory.amazonCart.items) {
+        cartFactory.amazonAddProduct(product, cartFactory.amazonCart)
+      } else {
+        cartFactory.amazonCreateCart(product);
+      }
+    };
 
     var block = $(window).height();
     var navbar = $('.navbar').height();
