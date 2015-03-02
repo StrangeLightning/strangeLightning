fcda5c65b04591913671d5464436ad536186385f
Scott Rice <scottrice10@gmail.com>
2015-03-02 10:48:31 -0800

    Move scheduler to product server

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ var config = require('./server/config/environment');
 var https = require('https');
 var fs = require('fs');
 var path = require('path');
-var schedule = require('node-schedule');
 
 var credentials = {
   key: process.env.NODE_ENV === 'production' ? fs.readFileSync(path.join(__dirname, '/../../shared/config/ssl.key'), 'utf-8') : fs.readFileSync('./shared/config/ssl.key', 'utf-8'),
@@ -23,48 +22,31 @@ var credentials = {
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
-// Populate DB with sample data
-if (config.seedDB) {
-  require('./server/config/seed');
-}
-
 // Setup server
 var app = express();
 var serverHTTPS = require('https').createServer(credentials, app);
 var serverHTTP = require('http').createServer(app);
 
-var rule = new schedule.RecurrenceRule();
-rule.minute = 42;
-
-if (process.env.NODE_ENV === 'production') {
-  serverHTTPS.listen(443, config.ip, function () {
+if(process.env.NODE_ENV === 'production') {
+  serverHTTPS.listen(443, config.ip, function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   });
-  serverHTTP.listen(80, config.ip, function () {
+  serverHTTP.listen(80, config.ip, function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   });
-
-  //schedule indexing of MongoDB
-  schedule.scheduleJob(rule, function () {
-
-  });
-} else if (process.env.NODE_ENV === 'development') {
-  serverHTTP.listen(9000, "localhost", function () {
+} else if(process.env.NODE_ENV === 'development') {
+  serverHTTP.listen(9000, "localhost", function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   });
 
-  serverHTTPS.listen(4430, "localhost", function () {
+  serverHTTPS.listen(4430, "localhost", function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
-
-  schedule.scheduleJob(rule, function () {
-
   });
 }
 
 // Redirect all requests to https
-app.all('*', function (req, res, next) {
-  if (req.protocol !== 'https') {
+app.all('*', function(req, res, next) {
+  if(req.protocol !== 'https') {
     res.redirect('https://' + req.get('host') + req.originalUrl);
   } else {
     next();
