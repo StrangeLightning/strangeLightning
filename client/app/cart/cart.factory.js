71e2c5f68254a18a423fd8f1c99f7efee3f5af74
Tole <Tole>
2015-03-07 20:50:06 -0800

    adding and storing pill data correctly

--- client/app/cart/cart.factory.js
+++ client/app/cart/cart.factory.js
@@ -1,5 +1,5 @@
 angular.module('thesisApp')
-  .factory('cartFactory', ['$http', 'Auth', 'localStorageService', '$rootScope', function($http, Auth, localStorageService, $rootScope) {
+  .factory('cartFactory', ['$http', 'localStorageService', '$rootScope', function($http, localStorageService, $rootScope) {
     var cart = {};
     cart.amazonCart = {};
     //all local storage of amazonItems will be on this object.
@@ -87,12 +87,10 @@ angular.module('thesisApp')
           console.log('Error: ', err);
         })
     };
-    //return the CartFactory object
 
     ////AMAZON CART FUNCTIONALITY
 
     cart.amazonGetCart = function(callback) {
-      //console.log("FROM FACTORY WHEN GETTING CART + HMAC ", cartId, HMAC)
 
       $http.post('/api/amazoncarts/get', {})
         .success(function(data) {
@@ -149,25 +147,23 @@ angular.module('thesisApp')
     };
 
     cart.amazonAddProduct = function(product, amazonCart) {
-      //console.log("A CART FROM ADD PRODCUT", amazonCart);\
       var newquantity;
       cart.amazonCart.items = cart.amazonCart.items || [];
+      //look for the product id of being updated item in the cart
       for (var i = 0; i < cart.amazonCart.items.length; i++) {
         if (product === cart.amazonCart.items[i]['productId']) {
+          //add one to the quantity of that item
           newquantity = cart.amazonCart.items[i]['quantity'] + 1;
+          //save the place in the cart to follow DRY pattern
           var currentItem = i;
+          //if the item was already there set flag to updated
           var updated = true
           break;
         }
       }
-      if (updated !== true) {
-        cart.amazonCart.items.push({
-          "productId": product,
-          "quantity": 1
-        });
-        currentItem = cart.amazonCart.items.length - 1;
-      }
-      //console.log('WHEN ADDING ITEM', amazonCart['CartId'], newquantity)
+      //if it is a brand new item push it into the cart
+
+      //call to the API to add 1 quantity of the item
       $http.post('/api/amazoncarts/modify', {
           'id': product,
           'productId': product,
@@ -176,13 +172,22 @@ angular.module('thesisApp')
           'Quantity': newquantity || 1
         })
         .success(function(data) {
+          //success indicates a call to the API had no error,
+          //WARNING: success only means that a call was made
+          //due to the limit of API calls/minute it might
+          //not update the cart so we run the check
           if (data['Quantity'] > cart.amazonCart['Qty']) {
             if (updated === true) {
               amazonCart.items[currentItem].quantity++;
-            };
+            } else {
+              cart.amazonCart.items.push({
+                "productId": product,
+                "quantity": 1
+              })
+            }
             cart.amazonCart['Qty'] = data['Quantity'];
+            //update local quantity
             cart.saveLocally(cart.amazonCart);
-
           }
         })
         .error(function(err) {
@@ -192,7 +197,6 @@ angular.module('thesisApp')
     };
 
     cart.amazonCreateCart = function(itemId) {
-      // console.log(Auth.getCurrentUser().id)
       console.log("CREATING CART")
       $http.post('/api/amazoncarts/create', {
           'id': itemId
@@ -218,7 +222,6 @@ angular.module('thesisApp')
     };
 
     cart.amazonClearCart = function() {
-      // console.log(Auth.getCurrentUser().id)
 
       $http.post('/api/amazoncarts/clear', {})
         .success(function(data) {
---
+++ client/app/cart/cart.factory.spec.js
@@ -0,0 +1,47 @@
+describe('cartFactory', function() {
+  var createCartFactory, $scope, $rootScope, $controller, $httpBackend;
+
+  beforeEach(function() {
+    module('thesisApp');
+
+    inject(function($rootScope, $controller, _$httpBackend_, cartFactory) {
+      $scope = $rootScope.$new();
+      $httpBackend = _$httpBackend_;
+      cartFactory = cartFactory
+        // $localStorageService = _$localStorageService_;
+      createCartCtrl = function() {
+        $controller('CartCtrl', {
+          $scope: $scope,
+        })
+      };
+      createCartCtrl()
+
+    })
+    $httpBackend.whenPOST('/api/amazoncarts/get').respond('200')
+  });
+  afterEach(function() {
+    $httpBackend.whenPOST('/api/amazoncarts/get').respond('200')
+    $httpBackend.whenGET('app/explore/explore.html').respond('301')
+    $httpBackend.flush()
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('controller functions in place', function() {
+    it("should have a scope variable", function() {
+
+      expect($scope).toBeDefined();
+    });
+
+    it("should have a getItems function function", function() {
+      expect($scope.getItems).toBeDefined();
+    });
+
+    it('should have a removeFromCart function ', function() {
+      expect($scope.removeFromCart).toBeDefined();
+    });
+    it('should have a checkout (goToAmazonCart) function', function() {
+      expect($scope.goToAmazonCart).toBeDefined();
+    })
+  });
+});
